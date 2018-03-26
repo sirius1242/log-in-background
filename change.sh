db67f1f625a2be9bc4f993e5c90b354d74505bb3
sirius <sirius.ustc@gmail.com>
2018-03-26 17:11:11 +0800

    fixed some bugs, and it seems can use now

--- change.sh
+++ change.sh
@@ -1,16 +1,19 @@
-#!/usr/bin/zsh
+#!/bin/bash
 
+dir=$(dirname $0)
 if [[ $# -eq 0 ]] ; then
 	echo "please choose the picture you want to change"
 	exit 0
 fi
-./extractgst.sh
+$dir/extractgst.sh
 
-workdir=$(HOME)/shell-theme
-filename="$(cd $(dirname $1); pwd)/$(basename $1)"
-cp gnome-shell-theme-gresource.xml $workdir/
-sed -i "s/filename/$filename/g" $workdir/gnome-shell-theme-gresource.xml
-sed -i "/#lockDialogGroup/,/^$/s/url(.*)/$filename/g" $workdir/gnome-shell.css
-sed -i "/#lockDialogGroup/,/^$/s/\ repeat/\ no-repeat/g" $workdir/gnome-shell.css
+filename=$(basename $1)
+filedir=${HOME}/shell-theme/theme
+cp $dir/gnome-shell-theme.gresource.xml $filedir/
+cp $1 $filedir
+cd $filedir
+sed -i "s#filename#$filename#g" gnome-shell-theme.gresource.xml
+sed -i "/#lockDialogGroup/,/^$/s#url(.*)#url($filename)#g" gnome-shell.css
+sed -i "/#lockDialogGroup/,/^$/s#\ repeat#\ no-repeat#g" gnome-shell.css
 glib-compile-resources gnome-shell-theme.gresource.xml
 sudo cp gnome-shell-theme.gresource /usr/share/gnome-shell
