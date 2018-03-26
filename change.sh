bed0a86d4929c45715f5c4f75050654ae5270dd6
sirius <sirius.ustc@gmail.com>
2018-03-26 17:32:51 +0800

    merge extractgst.sh as a function to change.sh

--- change.sh
+++ change.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
+extractgst() {
+	#source: https://wiki.archlinux.org/index.php/GDM#Log-in_screen_background_image
+	workdir=${HOME}/shell-theme
+	if [ ! -d ${workdir}/theme ]; then
+		mkdir -p ${workdir}/theme/icons
+	fi
+	gst=/usr/share/gnome-shell/gnome-shell-theme.gresource
+
+	for r in `gresource list $gst`; do
+					gresource extract $gst $r >$workdir/${r#\/org\/gnome\/shell/}
+	done
+}
 
 dir=$(dirname $0)
 if [[ $# -eq 0 ]] ; then
 	echo "please choose the picture you want to change"
 	exit 0
 fi
-$dir/extractgst.sh
+
+extractgst
 
 filename=$(basename $1)
-filedir=${HOME}/shell-theme/theme
-cp $dir/gnome-shell-theme.gresource.xml $filedir/
-cp $1 $filedir
-cd $filedir
+workdir=${HOME}/shell-theme/theme
+cp $dir/gnome-shell-theme.gresource.xml $workdir/
+cp $1 $workdir
+cd $workdir
 sed -i "s#filename#$filename#g" gnome-shell-theme.gresource.xml
 sed -i "/#lockDialogGroup/,/^$/s#url(.*)#url($filename)#g" gnome-shell.css
 sed -i "/#lockDialogGroup/,/^$/s#\ repeat#\ no-repeat#g" gnome-shell.css
